69ef99ae4b971f2d38d00a4bd125cceb3bd5ca6f
4nth0ny1 <acatullo4@gmail.com>
2021-12-10 16:03:34 -0600

    add basic routes and simple nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { Outlet, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Anthony Catullo Portfolio</h1>
-    </div>
+    <>
+      <nav>
+        <Link to="/">Home</Link> 
+        <Link to="/about">About</Link> 
+        <Link to="/blog">Blog</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <div className="hero">
+        <h1>Anthony Catullo Portfolio</h1>
+      </div>
+      <Outlet />
+    </>
   );
 }
-
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import ReactDOM from 'react-dom';
+import App from "./App";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Blog from "./routes/Blog";
+import Projects from "./routes/Projects";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="projects" element={<Projects />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
