846f65a3b70125bfc3dfacfde6e498fcb4dacd08
4nth0ny1 <acatullo4@gmail.com>
2021-12-10 20:25:06 -0600

    add hero section image

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,29 @@
 export default function Home() {
     return (
       <main>
-        <h1>Anthony Catullo Portfolio</h1>
+        <div className="hero">
+          <div className="hero-message">
+            <h1>Anthony Catullo</h1>
+            <h1>Portfolio</h1>
+            <h2>You have projects. I can code it.</h2>
+            <p>I'm a full stack developer in many languages and would love to code your next project.</p>
+          </div>
+          <img className="hero-image" src='programming.svg' alt="programming.svg" />
+        </div>
+        <div className="language-container">
+          <h2>Languages, Libraries, and Frameworks I love...</h2>
+          <p>Ruby</p>
+          <p>Ruby on Rails</p>
+          <p>Sinatra</p>
+          <p>ReactJS</p>
+          <p>JavaScript</p>
+          <p>Ruby</p>
+          <p>Bootstrap</p>
+          <p>Bootstrap</p>
+          <p>Bootstrap</p>
+          <p>Bootstrap</p>
+
+        </div>
       </main>
     );
   }
