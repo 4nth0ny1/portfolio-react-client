5e6e8599f20dace732bc6a50d5357b8f953b97d6
4nth0ny1 <acatullo4@gmail.com>
2021-12-10 20:05:56 -0600

    add contact form

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from '../components/ProjectCard';
 export default function Projects() {
     return (
       <main>
-        <h2>Projects</h2>
+        <h2 className='project-header'>Projects</h2>
         <div className="project-card-container">
           <ProjectCard />
           <ProjectCard />
