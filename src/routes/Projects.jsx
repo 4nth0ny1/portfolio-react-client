32e538a92b5564889ed76a5242a5aaa431461529
4nth0ny1 <acatullo4@gmail.com>
2021-12-10 18:03:36 -0600

    removed light toggle button and changed projects to cards

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,7 +1,14 @@
+import ProjectCard from '../components/ProjectCard';
+
 export default function Projects() {
     return (
       <main>
         <h2>Projects</h2>
+        <div className="project-card-container">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
       </main>
     );
   }
