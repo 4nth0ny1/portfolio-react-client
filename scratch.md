3dbace1f8c7dd325c3544218f48fce9199939a56
4nth0ny1 <acatullo4@gmail.com>
2021-12-15 21:08:57 -0600

    add styles to about page

--- scratch.md
+++ scratch.md
@@ -3,9 +3,12 @@ About page
 2-3 sentences about me
     I'm a former salesman, turned software developer. 
 
-    It started as a hobby and something I thought I could do as a side hustle. 3 years ago I was a marketer. I build my own wordpress website and started blogging for different companies, but I realized that 
+    It started as a hobby and something I thought I could do as a side hustle. 3 years ago I was a marketer. I build my own wordpress website and started blogging for different companies, but I realized 
 
 Where I'd like to be in 5 years
+<p>I feel like having a vision is very important. In 5 years, I can see myself coding some amazing projects and making a difference in the lives of my customers. </p>
+
+<p>I love projects that I'm emotionally attached to. </p>
 
 What I'd like my first job to be like
 
@@ -16,21 +19,3 @@ Technologies I know
 Technologies I'm currently learning 
 
 
-pros
-Express has a powerful routing syntax that supports dynamic routing patterns and is great for simple full stack applications. 
-
-The problem comes in when we want to perform async controller functions. We end up either overfetching or underfetch information because of the time it takes for the promise to be completed. 
-
-When you require controller async functionality there are better options including GraphQL, Fastify or Koa with Koa-Router or the npm package express-async-handler. 
-
-
-
-Express has a powerful routing syntax that supports dynamic routing patterns and is great for simple full stack applications. 
-
-The problem comes in when we want to perform async controller functions. We end up either overfetching or underfetch information because of the time it takes for the promise to be completed. 
-
-When you require controller async functionality there are better options including GraphQL, Fastify or Koa with Koa-Router or the npm package express-async-handler. 
-
-
-
-I say yes, they should implement a solution like auth0 because of the cost it will take implement your own. I think the risk of data breaches, the cost to build/maintain the system, and the cost of pentration testing will easily justify the costs even beyond the 10,000 user threshold. 
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,35 +3,65 @@
 export default function About() {
     return (
       <>
-        <div className="about">
-          <h2 className="about-header">Who am I?</h2>
-          <p>I'm a former salesman and marketer, turned software developer, and I'm currently looking for a position with a great team of developers that want to do great things.</p>
+        <div className="about about-first-section">
+          <div className="about-top-text">
+            <h2 className="about-header">Who am I?</h2>
+
+            <p>I'm a former salesman and marketer, turned software developer, and I'm currently looking for a position with a great team of developers that want to do great things.</p>
+
+            <br></br>
+
+            <p>This may sound corny, but I really feel like programmers have the power to shape the world. I think that even the smallest program can have an immense impact. I want to be part of a team that embodies that kind of mentality.</p>
+          </div>
+          <br></br>
+          <img className="computers" src="/computers.jpg" alt="computers" />
+        </div>
+
+
+
+        <div className="about about-final-section">
+          <div className="five-year-text">
+            <div className="future-about">
+            <h3 className="about-subheader">My ideal position</h3>
+              <hr className="horizontal"></hr>
+              <br></br>             
+              <p>A full stack developer that focuses on Ruby on Rails or ReactJS/NodeJS. I love both and would love an opportunity to make some great features and learn from an experienced team.</p>
+            </div>
+
+            <br></br>
+            <div className="future-about">
+              <h3 className="about-subheader">Five years from now</h3>
+              <hr></hr>
+              <br></br>
+              <p>I feel like having a vision is very important. In 5 years, I can see myself coding some amazing projects and making a difference in the lives of my customers.</p>
+            </div>
+
+
+          </div>
+          <br></br>
+
+        </div>
+
+        <div className='about'>
+          <p className="closing-line">If you like what you're reading so far, click on the <a href='/LinkPage'>links page</a> and shoot me an email. I'd love to talk to you about my projects in depth.</p>
         </div>
 
         <div className="about">
-          <h3 className="about-subheader">My Why</h3>
-          <p>This may sound corny, but I really feel like programmers have the power to shape the world. I think that even the smallest program can have an immense impact. I want to be part of a team that embodies that kind of mentality.</p>
+          <h3 className="about-subheader">Technology</h3>
+
+          <p>My favorite way of thinking about technologies is "tools in a toolbox." What is the best tool for this particular job? I guess this makes me language neutral.</p>
+
+          <br></br>
+
+          <p>I started with Ruby, Sinatra, and Ruby on Rails, which I think are fine technologies. They're really easy to learn and a great starting point for any new developer. I wish to continue to with these languages, while continuing to expand my repetoire.</p>
+
+          <br></br>
+
+          <p>These days I find myself neck deep in JavaScript. I love how much it's grown over the years. Now I spend most of time with ReactJS, NodeJS, Express, and MySQL. They are a newer part of my overall tech stack, but I love their simplicity and their scalability.</p>
+
         </div>
 
-        <div className="about">
-          <h3 className="about-subheader">How I learn new technologies</h3>
-          <p>New technology is your friend.</p>
-        </div>
 
-        <div className="about">
-          <h3 className="about-subheader">The kind of role I'm looking for</h3>
-          <p>New technology is your friend.</p>
-        </div>
-
-        <div className="about">
-          <h3 className="about-subheader">Technologies I know</h3>
-          <p>New technology is your friend.</p>
-        </div>
-
-        <div className="about">
-          <h3 className="about-subheader">Where I see myself in 5 years</h3>
-          <p>New technology is your friend.</p>
-        </div>
   
       </>
     );
